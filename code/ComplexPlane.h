6626b7b10c81191a98d0d686fbbe911d1e8299c8
Ahkar <ahkarminhein@gmail.com>
2025-04-30 10:28:13 -0700

    Last Update ComplexPlane.h, Ahkar

--- code/ComplexPlane.h
+++ code/ComplexPlane.h
@@ -2,7 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include <sstream>
 #include <complex>
-const unsigned int MAX_ITER = 200;
+const unsigned int MAX_ITER = 64;
 const float BASE_WIDTH = 4.0;
 const float BASE_HEIGHT = 4.0;
 const float BASE_ZOOM = 0.5;
